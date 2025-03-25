1de666b883a21e9984b2779a0641eda38b5ca0e0
Luipy56 <yoelberjaga@gmail.com>
2025-03-25 12:34:00 +0100

    Initial commit

---
+++ src/main/java/net/luipy/endalite/EndaliteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.luipy.endalite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EndaliteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}j
+}
